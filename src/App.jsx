f9582a8862574cd657dbefcbb6d433902132c9c2
cccrespo19 <140754478+cccrespo19@users.noreply.github.com>
2023-09-28 19:44:00 -0500

    Create working memory game Basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,157 @@
-import { useState } from 'react'
-import Card from './components/Card'
+import { useState, useEffect } from 'react';
+import Card from './components/Card';
 
 function App() {
+  const [dataAPI, setDataAPI] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [indices, setIndices] = useState([
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+  ]);
+  const [clickedNames, setClickedNames] = useState([]);
+  const [hasLost, setHasLost] = useState(0);
+  const [score, setScore] = useState(0);
+  const [lastScore, setLastScore] = useState(0);
+  const [bestScore, setBestScore] = useState(() => {
+    const localBestScore = localStorage.getItem('bestScore');
+    if (localBestScore) {
+      return Number(localBestScore);
+    } else {
+      return 0;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem('bestScore', bestScore.toString());
+  }, [bestScore]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        let pokemonArr = [];
+        let randomNums = [];
+
+        while (randomNums.length < 12) {
+          let randomID = Math.floor(Math.random() * 150 + 1).toString();
+
+          if (!randomNums.includes(randomID)) {
+            randomNums.push(randomID);
+          }
+        }
+
+        for (let i = 0; i < randomNums.length; i++) {
+          let response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${randomNums[i]}/`
+          );
+          let actualData = await response.json();
+          pokemonArr.push(actualData);
+        }
+
+        setDataAPI(pokemonArr);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        setDataAPI(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, [hasLost]);
+
+  if (error !== null) {
+    console.log(error);
+  }
+
+  const generateRandomIndices = () => {
+    let randomIndices = [];
+    while (randomIndices.length < 12) {
+      let randomIndex = Math.floor(Math.random() * 12);
+      if (!randomIndices.includes(randomIndex)) {
+        randomIndices.push(randomIndex);
+      }
+    }
+    setIndices(randomIndices);
+  };
+
+  const chooseCard = (e) => {
+    let button = e.target;
+    if (e.target.nodeName !== 'BUTTON') {
+      button = e.target.parentElement;
+    }
+
+    let pokemonName = button.querySelector('p').textContent;
+
+    if (!clickedNames.includes(pokemonName)) {
+      let clickedCopy = [...clickedNames];
+      clickedCopy.push(pokemonName);
+      setClickedNames(clickedCopy);
+
+      setScore(score + 1);
+
+      generateRandomIndices();
+    } else {
+      if (score > bestScore) {
+        setBestScore(score);
+      }
+
+      document.querySelector('dialog').showModal();
+
+      setHasLost(hasLost + 1);
+      setClickedNames([]);
+      setLastScore(score);
+      setScore(0);
+    }
+  };
+
+  const playAgain = () => {
+    document.querySelector('dialog').close();
+  };
+
   return (
     <>
       <header>
         <h1>Pokemon Memory Game</h1>
         <div className="scoreboard">
-          <p>Score: 0</p>
-          <p>Best score: 0</p>
+          <p>Score: {score.toString()}</p>
+          <p>Best score: {bestScore.toString()}</p>
         </div>
       </header>
       <main>
-        <p>Get points by clicking on an image, but don&apos;t click on any more than once!</p>
+        <p>
+          Get points by clicking on an image, but don&apos;t click on the same one twice!
+        </p>
 
         <div className="card-container">
-          <Card />
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              {Array(12)
+                .fill(<Card />)
+                .map((component, index) => {
+                  return (
+                    <Card
+                      key={dataAPI[index].name}
+                      dataAPI={dataAPI}
+                      index={indices[index]}
+                      chooseCard={chooseCard}
+                    />
+                  );
+                })}
+            </>
+          )}
         </div>
+
+        <dialog>
+          <h2>You lost!</h2>
+          <p>Your score: {lastScore}</p>
+          <button onClick={playAgain}>Play again</button>
+        </dialog>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,37 +1,17 @@
-import { useEffect, useState } from 'react';
-
-function Card() {
-  const [dataAPI, setDataAPI] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await fetch(
-          'https://pokeapi.co/api/v2/pokemon/ditto/'
-        );
-
-        let actualData = await response.json();
-        console.log(actualData);
-        console.log(actualData.sprites.front_default)
-        setDataAPI(actualData);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setDataAPI(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getData();
-  }, []);
+function Card({ dataAPI, index, chooseCard }) {
+  let spriteURL = '';
+  let name = '';
+  if (dataAPI.length !== 0) {
+    spriteURL = dataAPI[index].sprites.front_default;
+    name = dataAPI[index].name;
+    name = `${name[0].toUpperCase()}${name.slice(1)}`;
+  }
 
   return (
-    <div className="card">
-      <img src={dataAPI ? dataAPI.sprites.front_default :''} alt="img" />
-      <p>{dataAPI.name}</p>
-    </div>
+    <button className="card" onClick={chooseCard}>
+      <img src={spriteURL} alt="img" />
+      <p>{name}</p>
+    </button>
   );
 }
 
